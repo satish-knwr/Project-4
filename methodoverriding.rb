df609dc44758b0d7ea78e801a78474deccf10685
satish.knwr <satish.knwr@gmail.com>
2015-04-03 09:21:49 +0545

    Repository Initiated

---
+++ methodoverriding.rb
@@ -0,0 +1,13 @@
+class AbstractKlass  
+  def welcome  
+    puts "#{hello} #{name}"  
+  end  
+end  
+  
+# A concrete class  
+class ConcreteKlass < AbstractKlass  
+  def hello; "Hello"; end  
+  def name; "Ruby students"; end  
+end  
+  
+ConcreteKlass.new.welcome
